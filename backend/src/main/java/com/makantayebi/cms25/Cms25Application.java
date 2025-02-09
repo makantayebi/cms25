1d218fb3f926de39ed0307d61e7c3b76e4bbfcf6
Makan <contact@makantayebi.com>
2025-02-09 11:57:25 +0100

    init backend using spring initializr. https://start.spring.io/

---
+++ backend/src/main/java/com/makantayebi/cms25/Cms25Application.java
@@ -0,0 +1,13 @@
+package com.makantayebi.cms25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cms25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cms25Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/makantayebi/cms25/Cms25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makantayebi.cms25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cms25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
